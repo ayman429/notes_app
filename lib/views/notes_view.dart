eca9035468b7eed1297ae250c739f4359d73194b
ayman429 <am5432@fayoum.edu.eg>
2024-12-15 16:09:27 +0200

    show model buttom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/add_note_buttom_sheet.dart';
 import 'widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -10,7 +11,12 @@ class NotesView extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => const AddNoteButtomSheet(),
+          );
+        },
       ),
       body: const NotesViewBody(),
     );
